bb5dcd45f8cf66b14bb11c4e13decc9a914cdfee
CrowdHailer <peterhsaxton@gmail.com>
2014-01-12 19:24:45 +0000

    Copied in tapmanager plugin

--- js/next.js
+++ js/next.js
@@ -34,7 +34,6 @@ var svgAgile = {
 		console.log(G.timeStamp);
 	},
 	release: function (evt) {
-		console.log(this);
 		svgAgile.activity('off');
 	},
 	getContainingGroup: function (node, containerId) {
@@ -79,7 +78,36 @@ svgAgile.plugins.swishly = {
 };
 
 //MouseWheel plugin
-
+svgAgile.plugins.tapManager = {
+	init: function (callbacks) {
+		svgAgile.gestureHandlers.tap = this.tapHandler;
+		
+		
+		if(!callbacks['DEFAULT']) {
+			callbacks['DEFAULT'] = this.TAP_DEFAULT;
+		}
+		if(!callbacks['FIRST']) {
+			callbacks['FIRST'] = this.TAP_FIRST;
+		}
+		if(!callbacks['FINALLY']) {
+			callbacks['FINALLY'] = this.TAP_FINALLY;
+		}
+		
+		this.callbacks = callbacks;
+	},
+	tapHandler: function (evt) {
+		var id = evt.target.id || 'DEFAULT'; //get id of element
+		var callbacks = svgAgile.plugins.tapManager.callbacks 
+		var callback = callbacks[id] || callbacks['DEFAULT']; //check for call back or again run DEFAUT
+		callbacks['FIRST'](evt);
+		callback(evt);
+		callbacks['FINALLY'](evt);
+		//console.log(Object.keys(evt.target)); SUPER USEFUL LINE REMEMBER
+	},
+	TAP_DEFAULT: function (evt) {},
+	TAP_FIRST: function (evt) {},
+	TAP_FINALLY: function (evt) {}
+};
 
 svgAgile.init('manoeuvrable-svg');
 svgAgile.plugins.swishly.init('station');
